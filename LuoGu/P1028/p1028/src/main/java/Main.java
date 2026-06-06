2794d01ce79f54c4eb7919973a154b93dd6fe249
251250118 <251250118@smail.nju.edu.cn>
2026-06-07 02:28:58 +0800

    递归版本

--- LuoGu/P1028/p1028/src/main/java/Main.java
+++ LuoGu/P1028/p1028/src/main/java/Main.java
@@ -1,30 +1,21 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
 
-        C c = new C();
-        int countOfN = c.count(n) ;
-
-        System.out.println(countOfN);
-    }
-
-}
-
-class C {
-    public int count(int n){
-        if(n == 1){
-            return 1;
+        int[] count = new int[n+1];
+        count[0] = 0;
+        count[1] = 1;
+        for(int i = 2; i <= n; i ++){
+            count[i] = 1;
+            for(int j = 0; j <= i/2; j ++){
+                count[i] += count[j];
+            }
         }
-        int t = 0;
-        int count = 1;
-        while(count <= n / 2){
-            t += count(count);
-            count ++;
-        }
-        return t + 1;
+
+        System.out.println(count[n]);
     }
 }
